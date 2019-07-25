415bb3f65c1c9bc44ddc2dfcd5fca17a905b2b19
LRC <729345519@qq.com>
2019-07-25 13:58:12 +0800

    idea上传githup测试

--- src/main/java/life/majiang/community/controller/gitTest.java
+++ src/main/java/life/majiang/community/controller/gitTest.java
@@ -3,5 +3,7 @@ package life.majiang.community.controller;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("测试git");
+        System.out.println("测试git");
+
     }
 }
