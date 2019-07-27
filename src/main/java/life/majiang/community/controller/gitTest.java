a5d888c6bc46ff4aa3d0d6e6f9cedd520eb6444f
LRC <729345519@qq.com>
2019-07-27 09:42:11 +0800

    实现index页面跳转

--- src/main/java/life/majiang/community/controller/gitTest.java
+++
@@ -1,9 +0,0 @@
-package life.majiang.community.controller;
-
-public class gitTest {
-    public static void main(String[] args) {
-        System.out.println("测试git");
-        System.out.println("测试git");
-
-    }
-}
